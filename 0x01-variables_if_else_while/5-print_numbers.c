b8e60226df9db5b89b630d5288c017fd9994697a
Olufemi Odunlami <olufemiodunlami@gmail.com>
2023-02-17 12:41:01 -0800

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print single digits in base 10 from 0
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d", a);
+	}
+	printf("\n");
+	return (0);
+}
