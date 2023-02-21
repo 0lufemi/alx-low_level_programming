115d57c33c94d2dcc658acc30112e48e64aa3dd5
Olufemi Odunlami <olufemiodunlami@gmail.com>
2023-02-21 15:45:04 -0800

    5. Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_sign - print sign of a number
+ * @n: character to be checked
+ * Return: 1 & + if n > 0, 0 & 0 if n = 0, else -1 & -
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+		_putchar('+');
+		return (1);
+	else if (n == 0)
+		_putchar('0');
+		return (0);
+}
