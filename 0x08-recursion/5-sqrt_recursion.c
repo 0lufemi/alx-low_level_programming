63aab9b50dc664b1316469ec455a3666d94d6223
Olufemi Odunlami <olufemiodunlami@gmail.com>
2023-03-08 17:10:56 -0800

    Task 5, Calculate natural square root of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 int actual_sqrt_recursion(int n, int i);
 
 /**
@@ -9,13 +10,11 @@ int actual_sqrt_recursion(int n, int i);
 
 int _sqrt_recursion(int n)
 {
-	int _sqrt_recursion(int n)
-	{
 		if (n < 0)
 			return (-1);
 		return (actual_sqrt_recursion(n, 0));
-	}
 }
+
 /**
  * actual_sqrt_recursion - recurses to find the natural square root of a number
  * @n: number to calculate
