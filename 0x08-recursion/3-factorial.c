91b7b4ac5d875158810d2b54a67be5685d34c4e7
Olufemi Odunlami <olufemiodunlami@gmail.com>
2023-03-08 16:18:40 -0800

    3. You mustn't be afraid to dream a little bigger, darling

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*factorial - function that writes the factorial of a number
+*@n: The number
+*Return: Factorial of n
+*/
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+	return
+		(n + factorial(n - 1));
+}
