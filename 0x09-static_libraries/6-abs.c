ca4288f8d45340206b69b534456fe7da5ac3c5a9
Olufemi Odunlami <olufemiodunlami@gmail.com>
2023-03-10 14:58:06 -0800

    Task 0, create static library

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - print absolute value of an integer
+ * @t: integer value
+ * Return: absolute value
+ */
+
+int _abs(int t)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
