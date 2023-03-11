a5093de775dc4d1f9d707364ebe7ddbd74514044
Olufemi Odunlami <olufemiodunlami@gmail.com>
2023-03-11 15:31:32 -0800

    Program that multiplies 2 integer arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - program that multiplies 2 numbers
+*@argc: argument count
+*@argv: argunment vector
+*Return: 0
+*/
+
+int main(int argc, char **argv)
+{
+	int a, b, mul;
+
+	a = atoi(*(argv + 1));
+	b = atoi(*(argv + 2));
+
+	mul = a * b;
+	/*(void)argc;*/
+
+	if (argc > 3 || argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+		printf("%d\n", mul);
+	return (0);
+}
