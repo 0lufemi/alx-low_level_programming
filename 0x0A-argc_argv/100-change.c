d6445879c08dd5303575ba2a0d8e69d9efdab199
Olufemi Odunlami <olufemiodunlami@gmail.com>
2023-03-15 12:25:33 -0700

    Returns a pointer to a 2D Array

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+*main - prints minimum number of coins to make change
+*@argc: argument count
+*@argv: argument vector
+*Return: 1 if number of arguments is not 1, 0 otherwise
+*/
+
+int main(int argc, char *argv[])
+{
+}
